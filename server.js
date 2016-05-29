bd6ded13951e7693616f46de5e64704fb10d3799
Paul Korepanov <paul.kore@gmail.com>
2016-05-28 23:59:41 -0400

    Update server-start message

--- server.js
+++ server.js
@@ -165,7 +165,7 @@ var SampleApp = function() {
     self.start = function() {
         //  Start the app on the specific interface (and port).
         self.app.listen(self.port, self.ipaddress, function() {
-            console.log('%s: Node server started on %s:%d ...',
+            console.log('%s: trakr-API node server started on %s:%d ...',
                         Date(Date.now() ), self.ipaddress, self.port);
         });
 
